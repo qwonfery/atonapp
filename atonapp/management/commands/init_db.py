5d2c591fdc0ec0fcc5f2e38d060d3717ad21bf0c
qwonfery <nikitavish@list.ru>
2024-06-20 22:33:13 +0300

    init_db fix

--- atonapp/management/commands/init_db.py
+++ atonapp/management/commands/init_db.py
@@ -3,5 +3,7 @@ from django.core.management import BaseCommand, call_command
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
+        call_command('makemigrations')
+        call_command('migrate')
         call_command('users_syntesis')
         call_command('clients_syntesis')
